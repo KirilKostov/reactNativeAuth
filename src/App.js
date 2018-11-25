b77dd0c8e7aeea7ccbe0286ea65a5956250d0287
Kiril Chitinov <k.chitinov@gmail.com>
2018-11-25 23:40:34 +0200

    Add LoginForm component and firebase lib

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+	componentWillMount() {
+		firebase.initializeApp({
+	    apiKey: "AIzaSyDPUBm77TqUr1TL75pw3woQUxiXf14W-qY",
+	    authDomain: "auth-4df6b.firebaseapp.com",
+	    databaseURL: "https://auth-4df6b.firebaseio.com",
+	    projectId: "auth-4df6b",
+	    storageBucket: "auth-4df6b.appspot.com",
+	    messagingSenderId: "852243151490"
+	  });
+	}
+
 	render() {
 		return (
 			<View>
 				<Header headerText="Authentication" />
-				<Text>An app</Text>
+				<LoginForm />
 			</View>
 		);
 	}
