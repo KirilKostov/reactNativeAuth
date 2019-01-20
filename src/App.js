7742f2d168235ffd5524a2e9559dca9ee9ce9fcf
Kiril Chitinov <k.chitinov@gmail.com>
2019-01-20 20:03:48 +0200

    Switch between logIn and logOut views on 'state' change

--- src/App.js
+++ src/App.js
@@ -1,29 +1,56 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner, CardSection } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
-	componentWillMount() {
-		firebase.initializeApp({
-	    apiKey: "AIzaSyDPUBm77TqUr1TL75pw3woQUxiXf14W-qY",
-	    authDomain: "auth-4df6b.firebaseapp.com",
-	    databaseURL: "https://auth-4df6b.firebaseio.com",
-	    projectId: "auth-4df6b",
-	    storageBucket: "auth-4df6b.appspot.com",
-	    messagingSenderId: "852243151490"
-	  });
-	}
+  state = {
+    loggedIn: null
+  };
 
-	render() {
-		return (
-			<View>
-				<Header headerText="Authentication" />
-				<LoginForm />
-			</View>
-		);
-	}
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyDPUBm77TqUr1TL75pw3woQUxiXf14W-qY",
+      authDomain: "auth-4df6b.firebaseapp.com",
+      databaseURL: "https://auth-4df6b.firebaseio.com",
+      projectId: "auth-4df6b",
+      storageBucket: "auth-4df6b.appspot.com",
+      messagingSenderId: "852243151490"
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <CardSection>
+            <Button>Log Out</Button>
+          </CardSection>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />
+    }
+  }
+
+  render() {
+    return (
+      <View>
+        <Header headerText="Authentication" />
+        {this.renderContent()}
+      </View>
+    );
+  }
 }
 
 export default App;
